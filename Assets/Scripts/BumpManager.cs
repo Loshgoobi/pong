514bc2ff6a7d1d37c6ae02196af376eb1eac18f0
Loshgoobi <remy.dozoul@ynov.com>
2018-04-14 00:42:50 +0200

    gameplay finished

---
+++ Assets/Scripts/BumpManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumpManager : MonoBehaviour {
+
+    public ScoreUI score;
+    public GameObject bump;
+    public Transform[] spawnPoints;
+
+	// Use this for initialization
+	void Start () {
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        InvokeRepeating("Spawn", 10, Random.Range(11, 25));
+    }
+	
+
+    void Spawn()
+    {
+        // Find a random index between zero and one less than the number of spawn points.
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // Create an instance of the bump prefab at the randomly selected spawn point's position and rotation.
+        Instantiate(bump, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
---
+++ Assets/Scripts/BumpScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumpScript : MonoBehaviour {
+
+    public float destroyTimer;
+
+    private void Awake()
+    {
+        Destroy(gameObject, destroyTimer);
+    }
+}
